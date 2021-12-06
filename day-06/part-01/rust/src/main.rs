fa8f8fe4c5042ed1e32b17eef87d57ff40244680
Aleksandr White <aleksandr@asdfjackal.com>
2021-12-06 17:00:30 -0500

    Day Six (Rust only)

---
+++ day-06/part-01/rust/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+  let data = fs::read_to_string("../input.txt").expect("Something went wrong reading the file");
+  let starting_fish: Vec<i32> = data.split(",").map(|fish| {
+      fish.parse().unwrap()
+  }).collect();
+
+  let mut fish_state : [u128; 9] = [0; 9];
+
+  for fish in starting_fish {
+    fish_state[fish as usize] += 1;
+  }
+
+  for _ in 1..=80 {
+    fish_state = process_day(fish_state);
+  }
+
+  println!("{}", fish_state.iter().sum::<u128>())
+}
+
+fn process_day(state: [u128; 9]) -> [u128; 9] {
+  let mut new_state = [0; 9];
+    for i in 1..9 {
+      new_state[i-1] = state[i];
+    }
+    new_state[6] += state[0];
+    new_state[8] += state[0];
+  new_state
+}
